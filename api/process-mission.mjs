6aedeb2cc4fb3a999fa3106a11d4d8849da92362
zeyadsayedinq <zeyadsayedinq@gmail.com>
2026-05-08 15:20:04 +0300

    v-test1

--- api/process-mission.mjs
+++ api/process-mission.mjs
@@ -1,5 +1,15 @@
 const CORS = {
   'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type',
+};
+
+function setCORS(res) {
+  Object.entries(CORS).forEach(([k, v]) => res.setHeader(k, v));
+}
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Content-Type': 'application/json',
